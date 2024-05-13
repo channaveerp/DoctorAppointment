0495046aa44fd5c1bc20135494ef136d28c6936f
channaveer <channaveerpatil999@gmail.com>
2024-05-13 08:55:51 +0530

    mongo db connection

--- backend/app.js
+++ backend/app.js
@@ -1,2 +1,22 @@
 import express from 'express';
+
+import cookieParser from 'cookie-parser';
+import fileUpload from 'express-fileupload';
+import { dbconnection } from './database/dbconnections.js';
+import { config } from 'dotenv';
+
+config();
+
 export const app = express();
+dbconnection();
+
+app.use(cookieParser);
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: '/temp/',
+  })
+);
